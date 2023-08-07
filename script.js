2be4e1561a49abf6552a7389dcc643ae7f529f4c
bhaskar1912 <bhaskarmishra1912@gmail.com>
2023-08-07 17:08:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,158 @@
+function change1()
+{
+
+    var element = document.getElementById("boxx2");
+    element.style.height = '150px';
+    element.style.backgroundColor = '#F4FBF9';
+    element.style.border='1px solid #007F61';
+    document.getElementById("pair11").style.marginTop='-120px'
+    document.getElementById("radio11").style.marginTop='-120px'
+    document.getElementById("off11").style.marginTop='-77px'
+    document.getElementById("price11").style.marginTop='-77px'
+     var style2 = document.getElementById("select11");
+     style2.style.display='block'
+     style2.style.marginLeft='-279px'
+     style2.style.marginTop='-25px'
+
+     var style22 = document.getElementById("select111");
+     style22.style.display='block'
+     style22.style.marginLeft='9px'
+     style22.style.marginTop='-25px'
+
+     document.getElementById("boxx3").style.border='1px solid #C8C8C8';
+     document.getElementById("boxx4").style.border='1px solid #C8C8C8';
+
+    // setting box 2 
+
+       document.getElementById("select22").style.display='none'
+       document.getElementById("select222").style.display='none'
+       document.getElementById("boxx3").style.backgroundColor = 'white';
+       document.getElementById("boxx3").style.height='74px'
+
+       document.getElementById("pair22").style.marginTop='-20px'
+       document.getElementById("radio22").style.marginTop='-20px'
+       document.getElementById("popular22").style.marginTop='-25px'
+       document.getElementById("off22").style.marginTop='15px'
+       document.getElementById("price22").style.marginTop='22px'
+       document.getElementById("cross22").style.marginTop='59px'
+
+       document.getElementById("select22").style.display='none'
+       document.getElementById("select22").style.display='none'
+
+     //  settng box 3
+     document.getElementById("select33").style.display='none'
+     document.getElementById("select333").style.display='none'
+     document.getElementById("boxx4").style.backgroundColor = 'white';
+     document.getElementById("boxx4").style.height='74px'
+ 
+     document.getElementById("pair33").style.marginTop='-20px'
+     document.getElementById("radio33").style.marginTop='-20px'
+     document.getElementById("off33").style.marginTop='7px'
+     document.getElementById("price33").style.marginTop='22px'
+    
+}
+
+
+
+function change2()
+{
+    var elementt = document.getElementById("boxx3");
+    elementt.style.height = '150px';
+    elementt.style.backgroundColor = '#F4FBF9';
+    elementt.style.border='1px solid #007F61';
+    document.getElementById("pair22").style.marginTop='-120px'
+    document.getElementById("radio22").style.marginTop='-120px'
+    document.getElementById("popular22").style.marginTop='-120px'
+    document.getElementById("off22").style.marginTop='-77px'
+    document.getElementById("price22").style.marginTop='-77px'
+    document.getElementById("cross22").style.marginTop='-34px'
+     var style2 = document.getElementById("select22");
+     style2.style.display='block'
+     style2.style.marginLeft='-279px'
+
+     var style22 = document.getElementById("select222");
+     style22.style.display='block'
+     style22.style.marginLeft='9px'
+
+     document.getElementById("boxx2").style.border='1px solid #C8C8C8';
+     document.getElementById("boxx4").style.border='1px solid #C8C8C8';
+
+
+    //  settng box1 
+    document.getElementById("select11").style.display='none'
+    document.getElementById("select111").style.display='none'
+    document.getElementById("boxx2").style.backgroundColor = 'white';
+    document.getElementById("boxx2").style.height='74px'
+
+    document.getElementById("pair11").style.marginTop='-20px'
+    document.getElementById("radio11").style.marginTop='-20px'
+    document.getElementById("off11").style.marginTop='7px'
+    document.getElementById("price11").style.marginTop='22px'
+
+
+    //  settng box 3
+    document.getElementById("select33").style.display='none'
+    document.getElementById("select333").style.display='none'
+    document.getElementById("boxx4").style.backgroundColor = 'white';
+    document.getElementById("boxx4").style.height='74px'
+
+    document.getElementById("pair33").style.marginTop='-20px'
+    document.getElementById("radio33").style.marginTop='-20px'
+    document.getElementById("off33").style.marginTop='7px'
+    document.getElementById("price33").style.marginTop='22px'
+}
+
+
+function change3()
+{
+    var elementtt = document.getElementById("boxx4");
+    elementtt.style.height = '150px';
+    elementtt.style.backgroundColor = '#F4FBF9';
+    elementtt.style.border='1px solid #007F61';
+    document.getElementById("pair33").style.marginTop='-120px'
+    document.getElementById("radio33").style.marginTop='-120px'
+    document.getElementById("off33").style.marginTop='-77px'
+    document.getElementById("price33").style.marginTop='-77px'
+     var style33 = document.getElementById("select33");
+     style33.style.display='block'
+     style33.style.marginLeft='-279px'
+     style33.style.marginTop='-25px'
+
+     var style33 = document.getElementById("select333");
+     style33.style.display='block'
+     style33.style.marginLeft='9px'
+     style33.style.marginTop='-25px'
+
+     document.getElementById("boxx2").style.border='1px solid #C8C8C8';
+     document.getElementById("boxx3").style.border='1px solid #C8C8C8';
+    
+   //  settng box1 
+   document.getElementById("select11").style.display='none'
+   document.getElementById("select111").style.display='none'
+   document.getElementById("boxx2").style.backgroundColor = 'white';
+   document.getElementById("boxx2").style.height='74px'
+
+   document.getElementById("pair11").style.marginTop='-20px'
+   document.getElementById("radio11").style.marginTop='-20px'
+   document.getElementById("off11").style.marginTop='7px'
+   document.getElementById("price11").style.marginTop='22px'
+
+
+     // setting box 2 
+
+     document.getElementById("select22").style.display='none'
+     document.getElementById("select222").style.display='none'
+     document.getElementById("boxx3").style.backgroundColor = 'white';
+     document.getElementById("boxx3").style.height='74px'
+
+     document.getElementById("pair22").style.marginTop='-20px'
+     document.getElementById("radio22").style.marginTop='-20px'
+     document.getElementById("popular22").style.marginTop='-25px'
+     document.getElementById("off22").style.marginTop='15px'
+     document.getElementById("price22").style.marginTop='22px'
+     document.getElementById("cross22").style.marginTop='59px'
+
+     document.getElementById("select22").style.display='none'
+     document.getElementById("select22").style.display='none'
+
+}
